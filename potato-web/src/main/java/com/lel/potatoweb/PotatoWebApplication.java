cd0685fb025e0261c9486ba002eee29fd6e87e6f
lel210958 <lel210958@gmail.com>
2022-09-07 07:04:09 +0800

    提交项目忽略信息

---
+++ potato-web/src/main/java/com/lel/potatoweb/PotatoWebApplication.java
@@ -0,0 +1,13 @@
+package com.lel.potatoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotatoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotatoWebApplication.class, args);
+    }
+
+}
---
+++ potato-web/src/test/java/com/lel/potatoweb/PotatoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lel.potatoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotatoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
